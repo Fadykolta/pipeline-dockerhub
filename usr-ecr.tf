3404fdb886525927643cfee4f2a8a230c555bef6
Freddy K. Hens <fadykoltahens@gmail.com>
2023-05-13 17:39:45 -0400

    terrworkflow

---
+++ usr-ecr.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_ecr_repository" "test" {
+  name = "images-pipeline"
+  image_tag_mutability = "MUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_iam_user" "githubactions" {
+  name = "githubactions"
+}
+
+resource "aws_iam_user_policy_attachment" "attach_ecr_policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryFullAccess"
+  user = aws_iam_user.githubactions.name
+}
